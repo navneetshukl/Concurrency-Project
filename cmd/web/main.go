1ad595e779f1293daccb6d7299f0fb626d0db2ad
Navneet shukla <navneetshukla824@gmail.com>
2024-04-02 14:29:49 +0530

    Written function for database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,23 @@
 package main
 
-const webPort="8080"
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
 
-func main(){
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const webPort = "8080"
+
+func main() {
 	// Connect to Database
+
+	db := initDB()
+	db.Ping()
 
 	// create sessions
 
@@ -16,4 +30,54 @@ func main(){
 	// setup email
 
 	// listen for web connections
+}
+
+func initDB() *sql.DB {
+	conn := connectToDB()
+
+	if conn == nil {
+		log.Panic("Can't connect to Database")
+	}
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	counts := 0
+
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+
+		if err != nil {
+			log.Println("Postgres not yet ready...")
+		} else {
+			log.Println("Connected to database!")
+			return connection
+		}
+
+		if counts > 10 {
+			return nil
+		}
+
+		log.Println("Backing off for 1 second ")
+		time.Sleep(1 * time.Second)
+		counts++
+		continue
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
