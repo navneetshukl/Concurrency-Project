8d2937e641e387c472ca709f98989a3bce263ed0
Navneet shukla <navneetshukla824@gmail.com>
2024-04-02 12:19:05 +0530

    Written dockerfile

---
+++ cmd/web/main.go
@@ -0,0 +1,19 @@
+package main
+
+const webPort="8080"
+
+func main(){
+	// Connect to Database
+
+	// create sessions
+
+	// create channels
+
+	// create waitgroup
+
+	// setup the application config
+
+	// setup email
+
+	// listen for web connections
+}
